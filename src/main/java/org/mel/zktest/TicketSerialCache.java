e5e3bc8f1e4bb1d1473162292f5a528845248dd3
Mel <79642742@qq.com>
2018-03-28 21:02:58 +0800

    a

--- src/main/java/org/mel/zktest/TicketSerialCache.java
+++ src/main/java/org/mel/zktest/TicketSerialCache.java
@@ -4,6 +4,7 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.atomic.AtomicValue;
 import org.apache.curator.framework.recipes.atomic.DistributedAtomicInteger;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +18,12 @@ public class TicketSerialCache {
 
 	private ZKParams params;
 
+	private CuratorFramework client;
+	
 	public TicketSerialCache(CuratorFramework client, String path, ZKParams params) {
 		this.path = path;
 		this.params = params;
+		this.client=  client;
 		this.counter = new DistributedAtomicInteger(client, path, new ExponentialBackoffRetry(params.getTimeout(), 3));
 	}
 
@@ -47,22 +51,30 @@ public class TicketSerialCache {
 
 	private DateTime lastTime;
 
-	private boolean isNextDay() {
+	private boolean isNextDay() throws Exception {
 		DateTime currentTime = new DateTime();
 		if (lastTime == null) {
 			lastTime = currentTime;
 			return false;
 		}
 
-		if (lastTime.getDayOfMonth() != currentTime.getDayOfMonth()) {
+		if (lastTime.getDayOfMonth() == currentTime.getDayOfMonth()) {
 			lastTime = currentTime;
-			return true;
+			return false;
 		}
 
-		return false;
+		return checkMTime(currentTime);
 
 	}
 
+	private boolean checkMTime(DateTime currentTime) throws Exception {
+		Stat stat = this.client.checkExists().forPath(this.path);
+		if (new DateTime(stat.getMtime()).getDayOfMonth() != currentTime.getDayOfMonth()) {
+			System.out.println("BITCH");
+			return true;
+		}
+		return false;
+	}
 	@Override
 	public String toString() {
 		return String.format("Counter of %s.", path);
